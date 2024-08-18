b9e6b7ed52b0d2dfabc957e18d3e779977e0d132
Siyona Agarwal <113793806+SiyonaAgarwa@users.noreply.github.com>
2024-08-18 23:59:43 +0530

    Add files via upload

---
+++ command.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Installing required Python packages...
+
+pip install pyaudio
+pip install pyttsx3
+pip install SpeechRecognition
+pip install wikipedia
+pip install pywhatkit
+pip install pyjokes
+pip install PyAutoGUI
+pip install requests
+pip install PyQt5
+
+echo Installation complete.
+pause
